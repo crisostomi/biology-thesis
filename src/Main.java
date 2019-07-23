49a8e665d24f02de6257ad9961d6474b305c6095
scacio <andrea.caciolai@gmail.com>
2019-07-23 17:19:45 +0200

    split Main into Execute and Configure

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,134 @@
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+
 import org.apache.commons.cli.*;
 
 public class Main {
+    private static String inputDir;
+    private static String outputSBMLDir;
+    private static String outputModelicaDir;
+    private static String configDir;
 
-    static private Options options = new Options();
-    static private ArrayList<String> ignoreCompartmentList = new ArrayList<>();
-    static private ArrayList<String> selectCompartmentList = new ArrayList<>();
+    private static boolean configure;
+    private static boolean execute;
+
+    private static Options options = new Options();
+    private static ArrayList<String> ignoreCompartmentList = new ArrayList<>();
+    private static ArrayList<String> selectCompartmentList = new ArrayList<>();
+
+    private static final String usage = "java Main inputSBMLfolder outputSBMLfolder [outputModelicaFolder] [configFolder] -c | -e [-ic | -sc]";
+
+
+    public static void main(String[] args) {
+
+        handleOptions(args);
+        handleArguments(args);
+
+        if (configure) {
+            Configure.run(inputDir, outputSBMLDir, configDir);
+        } else {
+            Execute.run(inputDir, outputSBMLDir, outputModelicaDir, configDir);
+        }
+
+
+        /*
+        TODO: add --configure and --execute operational modes
+        TODO: fix ignoring compartments trouble with reaction
+         */
+
+    }
+
+    private static void handleArguments(String[] args) {
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            String[] argcopy = cmd.getArgs();
+
+            /*
+            0. input folder with sbml files
+            1. output folder to write sbml_union
+            2. output folder to write Modelica files (BioSystem + Monitor)
+            3. input/output folder with config file
+            */
+
+            inputDir = argcopy[0];
+            outputSBMLDir = argcopy[1];
+
+            if (argcopy.length <= 2) outputModelicaDir = argcopy[1];
+            if (argcopy.length <= 3) configDir = argcopy[1];
+
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp(usage, options);
+
+            System.exit(1);
+        }
+    }
+
 
     /**
-     * Method to add an option to the cmd invocation of the program
-     * @param opt short name of the option (generally a letter), called with a -
-     * @param longOpt long name of the option, called with a --
-     * @param hasArg whether the option is a simple flag or has an argument after it
-     * @param description short description of the use and purpose of the option
-     * @param isRequired whether or not the option is, well, optional or not
+     * Method to handle options to the command-line invokation of the program
+     * @param args the arguments to the terminal
      */
-    private static void addOption(String opt, String longOpt, boolean hasArg,
-                                  String description, boolean isRequired) {
-        Option option = new Option(opt, longOpt, hasArg, description);
-        option.setRequired(isRequired);
-        options.addOption(option);
+    private static void handleOptions(String[] args) {
+        addOption("ic", "ignoreComp", true,
+                "list of compartment ids to be ignored {compId_1, ..., compId_n}", false);
+
+        addOption("sc", "selectComp", true,
+                "list of compartment ids to be selected {compId_1, ..., compId_n}", false);
+
+        addOption("c", "configure", false,
+                "\'configure\' mode: only create configuration file structure and sbml union", false);
+
+        addOption("e", "execute", false,
+                "\'execute\' mode: create sbml union with configuration file in input", false);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+            configure = cmd.hasOption("c");
+            execute = cmd.hasOption("e");
+
+
+            if (!configure && !execute) {
+                System.out.println("Please specify a mode.");
+                formatter.printHelp(usage, options);
+
+                System.exit(1);
+            }
+
+            if (configure && execute) {
+                System.out.println("Cannot specify both modes at once.");
+                formatter.printHelp(usage, options);
+
+                System.exit(1);
+            }
+
+            if (cmd.hasOption("ic")) {
+                ignoreCompartmentList.addAll(parseOptionList(cmd, "ignoreComp"));
+                if (cmd.hasOption("sc")) {
+                    System.out.println("Cannot specify both compartments to be ignored and to be selected at once.");
+                    formatter.printHelp("utility-name", options);
+
+                    System.exit(1);
+                }
+            }
+            else if (cmd.hasOption("sc")) {
+                selectCompartmentList.addAll(parseOptionList(cmd, "selectComp"));
+            }
+
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp(usage, options);
+
+            System.exit(1);
+        }
     }
 
     /**
@@ -52,114 +157,18 @@ public class Main {
         return out;
     }
 
-    public static void main(String[] args) {
-
-        /*
-        0. input folder with sbml files
-        1. output folder to write sbml_union
-        2. output folder to write Modelica files (BioSystem + Monitor)
-        3. input config file
-        */
-        String[] argcopy = new String[4];
-        System.arraycopy(args, 0, argcopy, 0, args.length);
-        if(argcopy[2] == null) argcopy[2] = argcopy[1];
-
-        String inputDir = argcopy[0];
-        String outputSBMLDir = argcopy[1];
-        String outputModelicaDir = argcopy[2];
-        String configXMLfile = argcopy[3];
-
-        /*
-        addOption(null, "ignoreComp", true,
-                "list of compartment ids to be ignored {compId_1, ..., compId_n}", false);
-
-        addOption(null, "selectComp", true,
-                "list of compartment ids to be selected {compId_1, ..., compId_n}", false);
-
-        CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd = null;
-
-        try {
-            cmd = parser.parse(options, args);
-            ignoreCompartmentIdList.addAll(parseOptionList(cmd, "ignoreComp"));
-            selectCompartmentIdList.addAll(parseOptionList(cmd, "selectComp"));
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            formatter.printHelp("utility-name", options);
-
-            System.exit(1);
-        }
-        */
-
-        HashSet<Compartment> comps;
-        Parser P;
-
-        // parse input sbml
-        try { //TODO: don't generate sbml union in this Main
-            P = new Parser(inputDir, outputSBMLDir);
-        } catch(ParserConfigurationException e){
-            System.out.println("Parsing fail due to SBMLBuilder instantiation failure");
-            return;
-        }
-
-        // convert sbml data into a collection of Compartment objects
-        comps = P.instantiateCompartments();
-        if (comps == null || comps.size() == 0) {
-            System.out.println("Compartment instantiation failed. Returning with error");
-            return;
-        }
-
-        /*
-        create a graph structure in order to find species that only get consumed or produced
-        by the system reactions
-         */
-        Graph g = BioToGraph.buildGraph(comps);
-        HashSet<Species> sinks = new HashSet<>();
-        for(Node node: g.findSinks()){
-            sinks.add(node.getSpecies());
-        }
-        HashSet<Species> sources = new HashSet<>();
-        for(Node node: g.findSources()){
-            sources.add(node.getSpecies());
-        }
-
-        // instantiate the Java model
-        BioSystem bs = new BioSystem(comps, sinks, sources);
-
-        // convert Java biosystem model in Modelica
-        ModelBuilder mb = new ModelBuilder(bs, outputModelicaDir);
-        try {
-            mb.buildBioSystem();
-        } catch(IOException e){
-            System.out.println("Modelica files creation/writing failed");
-        }
-
-        String xmlOutDir = "/home/scacio/Dropbox/Tesisti/software/development/reactome-compiler/test-case-6/out";
-
-        try {
-            ConstraintBuilder cb = new ConstraintBuilder(bs, xmlOutDir);
-            cb.build();
-        } catch (Exception e) {
-            System.out.println("Constraints XML creation/writing failed");
-            e.printStackTrace();
-        }
-
-        MonitorBuilder monb = new MonitorBuilder(bs, xmlOutDir, outputModelicaDir);
-        try {
-            monb.build();
-        } catch (IOException e) {
-            System.out.println("Modelica monitor creation/writing failed");
-            e.printStackTrace();
-        }
-
-        System.out.println("All done!");
-
-
-        /*
-        TODO: add --configure and --execute operational modes
-        TODO: fix ignoring compartments trouble with reaction
-         */
-
+    /**
+     * Method to add an option to the cmd invocation of the program
+     * @param opt short name of the option (generally a letter), called with a -
+     * @param longOpt long name of the option, called with a --
+     * @param hasArg whether the option is a simple flag or has an argument after it
+     * @param description short description of the use and purpose of the option
+     * @param isRequired whether or not the option is, well, optional or not
+     */
+    private static void addOption(String opt, String longOpt, boolean hasArg,
+                                  String description, boolean isRequired) {
+        Option option = new Option(opt, longOpt, hasArg, description);
+        option.setRequired(isRequired);
+        options.addOption(option);
     }
 }
